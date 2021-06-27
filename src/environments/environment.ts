9b357f330f3fae9e2049f4c702509638c74b0b72
hugueds <hugo-pdf@hotmail.com>
2021-06-27 18:52:48 -0300

    updating language

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,14 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyBa6gaCyKpnn3jkGzuLhHPuMm_ODRe-CVQ",
-    authDomain: "firestarter-demo-3c120.firebaseapp.com",
-    databaseURL: "https://firestarter-demo-3c120.firebaseio.com",
-    projectId: "firestarter-demo-3c120",
-    storageBucket: "firestarter-demo-3c120.appspot.com",
-    messagingSenderId: "943791726198",
-    appId: "1:943791726198:web:0fffe5798c80b34f2bc957"
+  apiKey: "AIzaSyCX8s8gxDYR7qYJqUFBryoH7PnmmidlGKo",
+    authDomain: "univesp-pi6-9b634.firebaseapp.com",
+    databaseURL: "https://univesp-pi6-9b634-default-rtdb.firebaseio.com",
+    projectId: "univesp-pi6-9b634",
+    storageBucket: "univesp-pi6-9b634.appspot.com",
+    messagingSenderId: "445454099147",
+    appId: "1:445454099147:web:de9fe888e612f861e3714e",
+    measurementId: "G-B98KYCMC4G"
   }
 };
 
